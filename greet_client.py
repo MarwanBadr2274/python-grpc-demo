93fe2a1232cb9d0476c37467d86612e50b2d990f
MarwanBadr2274 <marwan.ayman.badr@gmail.com>
2024-03-02 19:07:33 +0200

    Initial commit

---
+++ greet_client.py
@@ -0,0 +1,53 @@
+import greet_pb2_grpc
+import greet_pb2
+import time
+import grpc
+
+def get_client_stream_requests():
+    while True:
+        name = input("Please enter a name (or nothing to stop chatting): ")
+
+        if name == "":
+            break
+
+        hello_request = greet_pb2.HelloRequest(greeting = "Hello", name = name)
+        yield hello_request
+        time.sleep(1)
+
+
+# function to run client
+def run():
+    # create channel
+    with grpc.insecure_channel('localhost:50051') as channel:
+        # create stub to use for making grpc calls
+        stub = greet_pb2_grpc.GreeterStub(channel)
+        print("1. SayHello - Unary")
+        print("2. ParrotSayHello - Server Side Streaming")
+        print("3. ChattyClientSayHello - Client Side Streaming")
+        print("4. InteractingHello - Bidirectional Streaming")
+        rpc_call = input("Which rpc would you like to call? : ")
+
+        if rpc_call == 1:
+            hello_request = greet_pb2.HelloRequest(greeting = "Bonjour", name = "Marwan")
+            hello_reply = stub.SayHello(hello_request)
+            print("SayHello Response Received:")
+            print(hello_reply)
+        elif rpc_call == 2:
+            hello_request = greet_pb2.HelloRequest(greeting="Bonjour", name="Marwan")
+            hello_replies = stub.ParrotSaysHello(hello_request)
+            for hello_reply in hello_replies:
+                print("ParrotSaysHello Response Received:")
+                print(hello_reply)
+        elif rpc_call == 3:
+            delayed_reply = stub.ChattyClientSaysHello(get_client_stream_requests())
+            print("ChattyClientSaysHello Response Received: ")
+            print(delayed_reply)
+        elif rpc_call == 4:
+            responses = stub.InteractingHello(get_client_stream_requests())
+
+            for response in responses:
+                print("InteractingHello Response Received: ")
+                print(response)
+
+if __name__ == "__main__":
+    run()
